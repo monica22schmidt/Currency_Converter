cc19acb0072991680e59ce1679912f42a129a6ca
monica22schmidt <monica22schmidt@gmail.com>
2020-02-06 19:15:31 -0600

    Null pounds error but almost there

---
+++ app/src/main/java/com/example/currencyconverter/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView2 = findViewById(R.id.textView2);
+        Intent intent = getIntent();
+        String pounds = intent.getStringExtra("message");
+        textView2.setText(pounds + " pounds");
+    }
+}
--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,9 +3,25 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    public void clickFunction(View view){
+        EditText myTextField = findViewById(R.id.editText);
+        String str = myTextField.getText().toString();
+        double dollars = (double) Integer.parseInt(str);
+        double pounds = dollars * .77;
 
+        goToActivity2(pounds);
+    }
+
+    public void goToActivity2(Double pounds){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("message", pounds);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
